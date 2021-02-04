f6a199f7f8909c288e364588a52270b849c22c3a
philbucher <philipp.bucher@tum.de>
2021-02-04 10:48:40 +0100

    beam using consistent MassMatrix by default

--- applications/StructuralMechanicsApplication/custom_elements/beam_element_3D2N.cpp
+++ applications/StructuralMechanicsApplication/custom_elements/beam_element_3D2N.cpp
@@ -1018,14 +1018,13 @@ void BeamElement3D2N::CalculateMassMatrix(MatrixType& rMassMatrix,
     }
     rMassMatrix = ZeroMatrix(msElementSize, msElementSize);
 
-    bool use_consistent_mass_matrix = false;
+    const bool compute_lumped_mass_matrix = StructuralMechanicsElementUtilities::ComputeLumpedMassMatrix(GetProperties(), rCurrentProcessInfo);
 
-    if (GetProperties().Has(USE_CONSISTENT_MASS_MATRIX)) {
-        use_consistent_mass_matrix = GetProperties()[USE_CONSISTENT_MASS_MATRIX];
+    if (compute_lumped_mass_matrix)  {
+        CalculateLumpedMassMatrix(rMassMatrix, rCurrentProcessInfo);
+    } else {
+        CalculateConsistentMassMatrix(rMassMatrix, rCurrentProcessInfo);
     }
-
-    if (use_consistent_mass_matrix) CalculateConsistentMassMatrix(rMassMatrix, rCurrentProcessInfo);
-    else CalculateLumpedMassMatrix(rMassMatrix, rCurrentProcessInfo);
 
     KRATOS_CATCH("")
 }
